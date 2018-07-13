eb351bce52411c376663c6bfa0c3021ce1a76d13
Heather Palmergit config --global user.email heatherpa@zillowgroup.com <palmerheather78@gmail.com>
2018-07-13 14:21:02 -0700

    My TEST FOR multiply() PASSES

--- class-05-images-color-text/lab/app.js
+++ class-05-images-color-text/lab/app.js
@@ -33,12 +33,19 @@ Write a function called multiply() that takes in two numbers as arguments and re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // Write your code here
-function multiply(a, b) { //eslint-disable-line
+function multiply(num1, num2) { //eslint-disable-line
+  var total = num1 * num2;
+  var array = [];
+  array.push(total);
 
+  var message = 'The product of ' + num1 + ' and ' + num2 + ' is ' + total + '.';
+  array.push(message);
+
+  return array;
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
