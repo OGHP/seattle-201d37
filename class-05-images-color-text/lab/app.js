0a19877de6738503976fbccbf46a009b1166e421
Heather Palmergit config --global user.email heatherpa@zillowgroup.com <palmerheather78@gmail.com>
2018-07-13 17:21:49 -0700

    My TEST FOR sumArray() PASSES

--- class-05-images-color-text/lab/app.js
+++ class-05-images-color-text/lab/app.js
@@ -81,7 +81,7 @@ function sumAndMultiply(num1, num2, num3) { //eslint-disable-line
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-testSumAndMultiply(4,7,5);
+// testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -98,13 +98,20 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 var testArray = [2, 3, 4]; //eslint-disable-line
 
-function sumArray(_sumArr) { //eslint-disable-line
+function sumArray([num1, num2, num3]) { //eslint-disable-line
+  var addNumbers = sum(num1, num2)[0];
+  var arrayTotal = sum(addNumbers, num3)[0];
+  var array = [];
+  array.push(arrayTotal);
 
+  var message = num1 + ',' + num2 + ',' + num3 + ' was passed in as an array of numbers, and ' + arrayTotal + ' is their sum.';
+  array.push(message);
+
+  return (array);
 }
 
 // Here is the test for sumArray(); uncomment it to run it
-
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
